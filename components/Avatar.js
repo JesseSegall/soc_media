d88f2834e6eb7d92fca19ef40b971538163c9185
Jesse <jsegall7@gmail.com>
2023-02-09 14:59:53 -0500

    Avatar component

---
+++ components/Avatar.js
@@ -0,0 +1,10 @@
+export default function Avatar() {
+	return (
+		<div className='w-12 rounded-full overflow-hidden '>
+			<img
+				src='https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZmFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60'
+				alt=''
+			/>
+		</div>
+	);
+}
