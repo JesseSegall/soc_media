f20b88c7c34fe767cdcda426c07e2e9db5b448b4
Jesse <jsegall7@gmail.com>
2023-02-09 15:00:28 -0500

    Updates

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
+import Avatar from '@/components/Avatar';
 import Card from '@/components/Card';
+import NavigationCard from '@/components/NavigationCard';
+import PostFormCard from '@/components/PostFormCard';
 
 export default function Home() {
 	return (
 		<div className='flex mt-4 max-w-4xl mx-auto gap-4'>
 			<div className='w-1/3'>
-				<Card>Test</Card>
+				<NavigationCard />
 			</div>
 			<div className='grow'>
 				<div className=''>
-					<Card>Form Here</Card>
-					<Card>First post test</Card>
+					<PostFormCard />
+					<Card>
+						<div className='flex'>
+							<div>
+								<Avatar />
+							</div>
+						</div>
+					</Card>
 				</div>
-				post
 			</div>
 		</div>
 	);
