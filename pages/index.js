1a8cf36a23aadc18d437c1874b05a9dbb546eeca
Jesse Segall <jsegall7@gmail.com>
2023-02-14 11:03:58 -0700

    Updated Index

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
 import Card from '@/components/Card';
 import NavigationCard from '@/components/NavigationCard';
+import PostCard from '@/components/PostCard';
 import PostFormCard from '@/components/PostFormCard';
 
 export default function Home() {
 	return (
 		<div className='flex mt-4 max-w-4xl mx-auto gap-4'>
-			<div className='w-1/3'>
+			<div className='w-1/4'>
 				<NavigationCard />
 			</div>
-			<div className='grow'>
+			<div className='w-3/4'>
 				<div className=''>
 					<PostFormCard />
-					<Card>
-						<div className='flex'>
-							<div>
-								<Avatar />
-							</div>
-						</div>
-					</Card>
+					<PostCard />
 				</div>
 			</div>
 		</div>
