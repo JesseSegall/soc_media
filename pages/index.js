8bcb82a31815057c1c1947ac16a89a44b7bdabd6
Jesse Segall <jsegall7@gmail.com>
2023-02-16 14:34:03 -0700

    added layout

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,13 @@
-import NavigationCard from '@/components/NavigationCard';
+import Layout from '@/components/Layout';
+
 import PostCard from '@/components/PostCard';
 import PostFormCard from '@/components/PostFormCard';
 
 export default function Home() {
 	return (
-		<div className='flex mt-4 max-w-4xl mx-auto gap-4'>
-			<div className='w-1/4'>
-				<NavigationCard />
-			</div>
-			<div className='w-3/4'>
-				<div className=''>
-					<PostFormCard />
-					<PostCard />
-				</div>
-			</div>
-		</div>
+		<Layout>
+			<PostFormCard />
+			<PostCard />
+		</Layout>
 	);
 }
