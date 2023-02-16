ca794983e0fcff9c4df9cfbde73b13f4a6b8a071
Jesse Segall <jsegall7@gmail.com>
2023-02-16 15:52:58 -0700

    updates

--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,13 @@
 import Avatar from '@/components/Avatar';
 import Card from '@/components/Card';
 import Layout from '@/components/Layout';
+import PostCard from '@/components/PostCard';
+import Link from 'next/link';
 
 export default function ProfilePage() {
+	const profileClasses = 'flex gap-1 px-3 py-1 items-center ';
+	const demoClass =
+		'flex gap-1 px-3 py-1 items-center border-socialBlue border-b-4 text-socialBlue';
 	return (
 		<Layout>
 			<Card noPadding={true}>
@@ -17,14 +22,85 @@ export default function ProfilePage() {
 						<Avatar size={'lg'} />
 					</div>
 
-					<div className='p-4 pb-24'>
+					<div className='p-4 '>
 						<div className='ml-40'>
 							<h1 className=' text-3xl font-bold'>John Doe</h1>
 							<div className='text-gray-500 leading-4'>New York, NY</div>
 						</div>
+						<div className='mt-10 flex gap-0'>
+							<Link href={'/'} className={demoClass}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='w-6 h-6'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25'
+									/>
+								</svg>
+								Posts
+							</Link>
+							<Link href={'/'} className={profileClasses}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='w-6 h-6'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z'
+									/>
+								</svg>
+								About
+							</Link>
+							<Link href={'/'} className={profileClasses}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='w-6 h-6'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z'
+									/>
+								</svg>
+								Friends
+							</Link>
+							<Link href={'/'} className={profileClasses}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									strokeWidth={1.5}
+									stroke='currentColor'
+									className='w-6 h-6'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										d='M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z'
+									/>
+								</svg>
+								Photos
+							</Link>
+						</div>
 					</div>
 				</div>
 			</Card>
+			<PostCard />
 		</Layout>
 	);
 }
