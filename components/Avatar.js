c5efeff9b22dba00dced22457e9a671ab31bc0ca
Jesse Segall <jsegall7@gmail.com>
2023-02-16 14:32:33 -0700

    added var to change size

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,6 +1,10 @@
-export default function Avatar() {
+export default function Avatar({ size }) {
+	let width = 'w-12';
+	if (size === 'lg') {
+		width = 'w-36';
+	}
 	return (
-		<div className='w-12 rounded-full overflow-hidden '>
+		<div className={`${width} rounded-full overflow-hidden`}>
 			<img
 				src='https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZmFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60'
 				alt=''
