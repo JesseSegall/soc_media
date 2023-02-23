beced4df525edb3972cb6f5b5f1e99e7bb2e12bf
Jesse <jsegall7@gmail.com>
2023-02-23 14:52:36 -0500

    Class updates for mobile

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -12,10 +12,8 @@ export default function NavigationCard() {
 		'text-sm md:text-md flex gap-1 md:gap-3 py-2 my-2 hover:bg-blue-500 hover:bg-opacity-20 md:-mx-4 px-6 md:px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300 items-center';
 	return (
 		<Card noPadding={true}>
-			<div className='px-4 py-2'>
-				<h2 className='text-gray-400 mb-3' font-bold>
-					Navigation
-				</h2>
+			<div className='px-4 py-2 flex justify-between md:block shadow-md shadow-gray-500 md:shadow-none'>
+				<h2 className='text-gray-400 mb-3 hidden md:block'>Navigation</h2>
 				<Link
 					href='/'
 					className={pathname === '/' ? activeElementClasses : nonActiveElementClasses}
@@ -34,7 +32,8 @@ export default function NavigationCard() {
 							d='M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25'
 						/>
 					</svg>
-					Home
+
+					<span className='hidden md:block'>Home</span>
 				</Link>
 				<Link
 					href='/profile/friends'
@@ -56,7 +55,7 @@ export default function NavigationCard() {
 							d='M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z'
 						/>
 					</svg>
-					Friends
+					<span className='hidden md:block'>Friends</span>
 				</Link>
 				<Link
 					href='/saved'
@@ -76,10 +75,13 @@ export default function NavigationCard() {
 							d='M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0120.25 6v12A2.25 2.25 0 0118 20.25H6A2.25 2.25 0 013.75 18V6A2.25 2.25 0 016 3.75h1.5m9 0h-9'
 						/>
 					</svg>
-					Saved Posts
+					<span className='hidden md:block'>Saved posts</span>
 				</Link>
-			
-				<Link href='' className={nonActiveElementClasses}>
+
+				<Link
+					href='/notifications'
+					className={pathname === '/notifications' ? activeElementClasses : nonActiveElementClasses}
+				>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
 						fill='none'
@@ -94,7 +96,7 @@ export default function NavigationCard() {
 							d='M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5'
 						/>
 					</svg>
-					Notifications
+					<span className='hidden md:block'>Notifications</span>
 				</Link>
 				<Link href='' className={nonActiveElementClasses}>
 					<svg
@@ -111,7 +113,7 @@ export default function NavigationCard() {
 							d='M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75'
 						/>
 					</svg>
-					Logout
+					<span className='hidden md:block'>Logout</span>
 				</Link>
 			</div>
 		</Card>
