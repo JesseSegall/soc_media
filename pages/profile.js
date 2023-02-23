5f8feb70776be00cd3c41c398e29720e405d121b
Jesse <jsegall7@gmail.com>
2023-02-23 14:54:27 -0500

    mobile updates

--- pages/profile.js
+++ pages/profile.js
@@ -15,9 +15,9 @@ export default function ProfilePage() {
 	const isFriends = pathname.includes('friends');
 	const isPhotos = pathname.includes('photos');
 
-	const tabClasses = 'flex gap-1 px-3 py-1 items-center border-b-4 border-b-white';
+	const tabClasses = 'flex gap-1 px-4 py-1 items-center border-b-4 border-b-white';
 	const activeTabClasses =
-		'flex gap-1 px-3 py-1 items-center border-socialBlue border-b-4 text-socialBlue';
+		'flex gap-1 px-4 py-1 items-center border-socialBlue border-b-4 text-socialBlue font-bold';
 	return (
 		<Layout>
 			<Card noPadding={true}>
@@ -53,7 +53,7 @@ export default function ProfilePage() {
 										d='M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25'
 									/>
 								</svg>
-								Posts
+								<span className='hidden sm:block'>Posts</span>
 							</Link>
 							<Link href={'/profile/about'} className={isAbout ? activeTabClasses : tabClasses}>
 								<svg
@@ -70,7 +70,7 @@ export default function ProfilePage() {
 										d='M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z'
 									/>
 								</svg>
-								About
+								<span className='hidden sm:block'>About</span>
 							</Link>
 							<Link href={'/profile/friends'} className={isFriends ? activeTabClasses : tabClasses}>
 								<svg
@@ -87,7 +87,7 @@ export default function ProfilePage() {
 										d='M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z'
 									/>
 								</svg>
-								Friends
+								<span className='hidden sm:block'>Friends</span>
 							</Link>
 							<Link href={'/profile/photos'} className={isPhotos ? activeTabClasses : tabClasses}>
 								<svg
@@ -104,7 +104,7 @@ export default function ProfilePage() {
 										d='M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z'
 									/>
 								</svg>
-								Photos
+								<span className='hidden sm:block'>Photos</span>
 							</Link>
 						</div>
 					</div>
