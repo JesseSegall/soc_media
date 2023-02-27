b38a36100414901195fb3d3f2270bc895a0d1b6f
Jesse <jsegall7@gmail.com>
2023-02-27 13:17:07 -0500

    images from db

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
 	function fetchPosts() {
 		supabase
 			.from('posts')
-			.select('id,content,created_at, profiles(id,avatar,name)')
+			.select('id,content,created_at,photos, profiles(id,avatar,name)')
 			.order('created_at', { ascending: false })
 			.then((result) => {
 				setPosts(result.data);
